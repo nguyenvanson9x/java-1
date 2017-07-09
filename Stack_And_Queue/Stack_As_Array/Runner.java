f82832b173b97d4207b2bac70627142980f2f640
Nguyen Van Son <asanfc9x@gmail.com>
2017-07-09 10:46:05 +0000

    Stack as Array

--- Stack_And_Queue/Stack_As_Array/Runner.java
+++ Stack_And_Queue/Stack_As_Array/Runner.java
@@ -1,10 +1,10 @@
 public class Runner {
     public static void main (String[] args) {
-        Stack_As_Array<Integer> s = new Stack_As_Array<Integer>(3);
+        Stack_As_Array<Integer> s = new Stack_As_Array<Integer>();
         s.push(5);
         s.push(12);
         s.push(30);
-        
+        s.push(15);
         s.print();
     }
 }
--- Stack_And_Queue/Stack_As_Array/Stack_As_Array.java
+++ Stack_And_Queue/Stack_As_Array/Stack_As_Array.java
@@ -2,11 +2,13 @@ public class Stack_As_Array<E> implements Stack<E> {
     private E[] stack;
     private int top = 0;
     
-    public Stack_As_Array(int cap) {
-        stack = (E[]) new Object[cap];
+    public Stack_As_Array() {
+        stack = (E[]) new Object[2];
     }
     
     public void push(E element) {
+        if (stack.length == top)
+            resize(stack.length * 2);
         stack[top] = element;
         top++;
     }
@@ -22,7 +24,13 @@ public class Stack_As_Array<E> implements Stack<E> {
     
     public void print() {
         int i;
-        for (i = 0; i < top; i++)
+        for (i = top - 1; i >= 0; i--)
             System.out.print(stack[i] + " ");
+    }
+    
+    private void resize(int cap) {
+        E[] tmp = (E[]) new Object[cap];
+        System.arraycopy(stack, 0, tmp, 0, top);
+        stack = tmp;
     }
 }
