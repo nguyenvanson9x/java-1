910be83a265d23a79c4fc2eabf441cbe26741b28
Nguyen Van Son <asanfc9x@gmail.com>
2017-07-28 14:52:34 +0000

    BST Exercise

--- Week_3/BinarySearchTree/OrderedSimpleTable_BST.java
+++ Week_3/BinarySearchTree/OrderedSimpleTable_BST.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrderedSimpleTable_BST<Key extends Comparable<Key>, Value>
-		implements OrderedSimpleTable<Key, Value> {
+public class OrderedSimpleTable_BST<Key extends Comparable<Key>, Value> implements OrderedSimpleTable<Key, Value> {
 	class Node {
 		Key key;
 		Value val;
@@ -61,8 +60,37 @@ public class OrderedSimpleTable_BST<Key extends Comparable<Key>, Value>
 
 	@Override
 	public void delete(Key key) {
-		put(key, null);
+		root = _delete(root, key);
 
+	}
+
+	private Node _delete(Node node, Key key) {
+		if (node == null)
+			return null;
+		int c = key.compareTo(node.key);
+		if (c < 0)
+			node.left = _delete(node.left, key);
+		else if (c > 0)
+			node.right = _delete(node.right, key);
+		else {
+			Node p = node;
+			if (node.left == null)
+				node = node.right;
+			else if (node.right == null)
+				node = node.left;
+			else {
+				Node s = node;
+				Node Q = s.left;
+				while (Q.right != null) {
+					s = Q;
+					Q = Q.right;
+				}
+				p.key = Q.key;
+				p.val = Q.val;
+				s.right = Q.left;
+			}
+		}
+		return node;
 	}
 
 	@Override
@@ -226,4 +254,388 @@ public class OrderedSimpleTable_BST<Key extends Comparable<Key>, Value>
 		return null;
 	}
 
+	// chiều cao của cây
+	public int depth() {
+		return _depth(root);
+	}
+
+	private int _depth(Node node) {
+		if (node == null)
+			return 0;
+		int d_left = _depth(node.left);
+		int d_right = _depth(node.right);
+		return 1 + (d_left > d_right ? d_left : d_right);
+	}
+
+	// xuất các key có giá trị chẵn
+	public Iterable<Key> keys_even() {
+		List<Key> l = new ArrayList<>();
+		_keys_even(root, l);
+		return l;
+	}
+
+	private void _keys_even(Node node, List<Key> l) {
+		if (node != null) {
+			_keys_even(node.left, l);
+			if ((Integer) node.key % 2 == 0)
+				l.add(node.key);
+			_keys_even(node.right, l);
+		}
+	}
+
+	// xuất các key có giá trị nằm trong đoạn [u, v]
+	public Iterable<Key> keys_between(Key u, Key v) {
+		List<Key> l = new ArrayList<>();
+		_keys_between(root, u, v, l);
+		return l;
+	}
+
+	private void _keys_between(Node node, Key u, Key v, List<Key> l) {
+		if (node != null) {
+			_keys_between(node.left, u, v, l);
+			if (u.compareTo(node.key) <= 0 && node.key.compareTo(v) <= 0)
+				l.add(node.key);
+			_keys_between(node.right, u, v, l);
+		}
+	}
+
+	// xuất các key nằm trên tầng thứ K của cây
+	public Iterable<Key> keys_level(int k) {
+		List<Key> l = new ArrayList<>();
+		_keys_level(root, k, l);
+		return l;
+	}
+
+	private void _keys_level(Node node, int k, List<Key> l) {
+		if (node != null) {
+			k--;
+			_keys_level(node.left, k, l);
+			if (k == 0)
+				l.add(node.key);
+			_keys_level(node.right, k, l);
+		}
+	}
+
+	// xuất tất cả các nút trên cây theo thứ tự từ tầng 0 đến tầng h-1 của cây (với
+	// h là chiều cao của cây)
+	public Iterable<Key> keys_all_level() {
+		List<Key> l = new ArrayList<>();
+		int depth = depth();
+		_keys_all_level(depth, root, l);
+		return l;
+	}
+
+	private void _keys_all_level(int depth, Node node, List<Key> l) {
+		int i;
+		for (i = 0; i < depth; i++)
+			_keys_level(node, i + 1, l);
+	}
+
+	// xuất các nút có đúng 1 con
+	public Iterable<Key> keys_only_one_child() {
+		List<Key> l = new ArrayList<>();
+		_keys_only_one_child(root, l);
+		return l;
+	}
+
+	private void _keys_only_one_child(Node node, List<Key> l) {
+		if (node != null) {
+			_keys_only_one_child(node.left, l);
+			_keys_only_one_child(node.right, l);
+			if ((node.left != null && node.right == null) || (node.left == null && node.right != null))
+				l.add(node.key);
+		}
+	}
+
+	// Đếm số lượng nút có đúng 1 con
+	public int count_key_only_one_child() {
+		return _count_key_only_one_child(root);
+	}
+
+	private int _count_key_only_one_child(Node node) {
+		if (node != null) {
+			int c_left = _count_key_only_one_child(node.left);
+			int c_right = _count_key_only_one_child(node.right);
+			if ((node.left != null && node.right == null) || (node.left == null && node.right != null))
+				return 1 + c_left + c_right;
+			return c_left + c_right;
+		}
+		return 0;
+	}
+
+	// Đếm số lượng nút có đúng 2 con
+	public int count_key_have_two_child() {
+		return _count_key_have_two_child(root);
+	}
+
+	private int _count_key_have_two_child(Node node) {
+		if (node != null) {
+			int c_left = _count_key_have_two_child(node.left);
+			int c_right = _count_key_have_two_child(node.right);
+			if (node.left != null && node.right != null)
+				return 1 + c_left + c_right;
+			return c_left + c_right;
+		}
+		return 0;
+	}
+
+	// Nút có đúng hai con
+	public Iterable<Key> keys_have_two_child() {
+		List<Key> l = new ArrayList<>();
+		_keys_have_two_child(root, l);
+		return l;
+	}
+
+	private void _keys_have_two_child(Node node, List<Key> l) {
+		if (node != null) {
+			_keys_have_two_child(node.left, l);
+			if (node.left != null && node.right != null)
+				l.add(node.key);
+			_keys_have_two_child(node.right, l);
+		}
+	}
+
+	// Đếm số lượng nút có key chẵn
+	public int count_even_key() {
+		return _count_even_key(root);
+	}
+
+	private int _count_even_key(Node node) {
+		if (node != null) {
+			int c_left = _count_even_key(node.left);
+			int c_right = _count_even_key(node.right);
+			if ((Integer) node.key % 2 == 0)
+				return 1 + c_left + c_right;
+			return c_left + c_right;
+		}
+		return 0;
+	}
+
+	// Đếm số lượng nút lá mà Key của nút đó là giá trị chẵn
+	public int count_even_leaf() {
+		return _count_even_leaf(root);
+	}
+
+	private int _count_even_leaf(Node node) {
+		if (node != null) {
+			int c_left = _count_even_leaf(node.left);
+			int c_right = _count_even_leaf(node.right);
+			if (node.left == null && node.right == null && (Integer) node.key % 2 == 0)
+				return 1 + c_left + c_right;
+			return c_left + c_right;
+		}
+		return 0;
+	}
+
+	// Xuất các lá có Key chẵn
+	public Iterable<Key> even_leaf() {
+		List<Key> l = new ArrayList<>();
+		_even_leaf(root, l);
+		return l;
+	}
+
+	private void _even_leaf(Node node, List<Key> l) {
+		if (node != null) {
+			_even_leaf(node.left, l);
+			if (node.left == null && node.right == null && (Integer) node.key % 2 == 0)
+				l.add(node.key);
+			_even_leaf(node.right, l);
+		}
+	}
+
+	// Đếm số lượng nút trên tầng thứ k của cây
+	public int count_level(int k) {
+		return _count_level(root, k);
+	}
+
+	private int _count_level(Node node, int k) {
+		if (node != null) {
+			k--;
+			int c_left = _count_level(node.left, k);
+			int c_right = _count_level(node.right, k);
+			if (k == 0)
+				return 1 + c_left + c_right;
+			return c_left + c_right;
+		}
+		return 0;
+	}
+
+	// tổng các Key trong cây
+	public int sum_keys() {
+		return _sum_keys(root);
+	}
+
+	private int _sum_keys(Node node) {
+		if (node != null) {
+			int sum_left = _sum_keys(node.left);
+			int sum_right = _sum_keys(node.right);
+			return (Integer) node.key + sum_left + sum_right;
+		}
+		return 0;
+	}
+
+	// tổng các Key ở nút lá
+	public int sum_leaf() {
+		return _sum_leaf(root);
+	}
+
+	private int _sum_leaf(Node node) {
+		if (node != null) {
+			int sum_left = _sum_leaf(node.left);
+			int sum_right = _sum_leaf(node.right);
+			if (node.left == null && node.right == null)
+				return (Integer) node.key + sum_left + sum_right;
+			return sum_left + sum_right;
+		}
+		return 0;
+	}
+
+	// tổng Key ở các nút có đúng một con
+	public int sum_keys_one_child() {
+		return _sum_keys_one_child(root);
+	}
+
+	private int _sum_keys_one_child(Node node) {
+		if (node != null) {
+			int sum_left = _sum_keys_one_child(node.left);
+			int sum_right = _sum_keys_one_child(node.right);
+			if ((node.left != null && node.right == null) || (node.left == null && node.right != null))
+				return (Integer) node.key + sum_left + sum_right;
+			return sum_left + sum_right;
+		}
+		return 0;
+	}
+
+	// Kiểm tra cây nhị phân T có phải là "cây nhị phân tìm kiếm" hay không?
+	public int is_BST() {
+		return _is_BST(root);
+	}
+
+	private int _is_BST(Node node) {
+		if (node == null)
+			return 0;
+		int check_left = _is_BST(node.left);
+
+		int maxL, maxR;
+		if (node.left != null && node.right != null) {
+			maxL = max_key(node.left);
+			maxR = max_key(node.right);
+			if (!(maxL < (Integer) node.key && (Integer) node.key < maxR))
+				return 1;
+		}
+		if (node.left != null && node.right == null) {
+			maxL = max_key(node.left);
+			if (!(maxL < (Integer) node.key))
+				return 1;
+		}
+		if (node.left == null && node.right != null) {
+			maxR = max_key(node.right);
+			if (!(maxR > (Integer) node.key))
+				return 1;
+		}
+
+		int check_right = _is_BST(node.right);
+		return check_left + check_right;
+	}
+
+	public int max_key(Node node) {
+		int max = -Integer.MAX_VALUE, max_L, max_R;
+		if (node != null) {
+			max = (Integer) node.key;
+			max_L = max_key(node.left);
+			max_R = max_key(node.right);
+			if (max_L > max)
+				max = max_L;
+			if (max_R > max)
+				max = max_R;
+		}
+		return max;
+	}
+
+	// Kiểm tra cây nhị phân T có phải là "cây nhị phân cân bằng" hay không?
+	// Cây nhị phân cân bằng là cây nhị phân tìm kiếm mà tại mỗi nút của nó
+	// độ cao của cây con trái và cây con phải chêch lệch ko quá 1
+	public int is_blanched_BST() {
+		return _is_blanched_BST(root);
+	}
+
+	private int _is_blanched_BST(Node node) {
+		if (node == null)
+			return 0;
+		int check_left = _is_blanched_BST(node.left);
+
+		int maxL, maxR;
+		if (node.left != null && node.right != null) {
+			maxL = max_key(node.left);
+			maxR = max_key(node.right);
+			if (!(maxL < (Integer) node.key && (Integer) node.key < maxR))
+				return 1;
+		}
+		if (node.left != null && node.right == null) {
+			maxL = max_key(node.left);
+			if (!(maxL < (Integer) node.key))
+				return 1;
+		}
+		if (node.left == null && node.right != null) {
+			maxR = max_key(node.right);
+			if (!(maxR > (Integer) node.key))
+				return 1;
+		}
+
+		int depth_left = _depth(node.left);
+		int depth_right = _depth(node.right);
+		if (Math.abs(depth_left - depth_right) > 1)
+			return 1;
+
+		int check_right = _is_blanched_BST(node.right);
+		return check_left + check_right;
+	}
+
+	// Kiểm tra cây nhị phân T có phải là "cây nhị phân cân bằng hoàn toàn" hay
+	// không?
+	// cây nhị phân cân bằng hoàn toàn là cây nhị phân tìm kiếm mà tại mỗi nút của
+	// nó, số nút của cây con trái và cây con phải chêch lệch ko quá 1
+	public int is_completely_balanced_BST() {
+		return _is_completely_balanced_BST(root);
+	}
+
+	private int _is_completely_balanced_BST(Node node) {
+		if (node == null)
+			return 0;
+		int check_left = _is_completely_balanced_BST(node.left);
+
+		int maxL, maxR;
+		if (node.left != null && node.right != null) {
+			maxL = max_key(node.left);
+			maxR = max_key(node.right);
+			if (!(maxL < (Integer) node.key && (Integer) node.key < maxR))
+				return 1;
+		}
+		if (node.left != null && node.right == null) {
+			maxL = max_key(node.left);
+			if (!(maxL < (Integer) node.key))
+				return 1;
+		}
+		if (node.left == null && node.right != null) {
+			maxR = max_key(node.right);
+			if (!(maxR > (Integer) node.key))
+				return 1;
+		}
+
+		int count_left = _count_keys(node.left);
+		int count_right = _count_keys(node.right);
+		if (Math.abs(count_left - count_right) > 1)
+			return 1;
+
+		int check_right = _is_completely_balanced_BST(node.right);
+		return check_left + check_right;
+	}
+
+	private int _count_keys(Node node) {
+		if (node == null)
+			return 0;
+		int c_left = _count_keys(node.left);
+		int c_right = _count_keys(node.right);
+		return 1 + c_left + c_right;
+	}
 }
--- Week_3/BinarySearchTree/Runner.java
+++ Week_3/BinarySearchTree/Runner.java
@@ -2,7 +2,7 @@ public class Runner {
 	public static void main(String[] args) {
 		// SimpleBST<Integer> s = new SimpleBST<>();
 		// SimpleTable_BST<Integer, Integer> s = new SimpleTable_BST<>();
-		OrderedSimpleTable<Integer, Integer> s = new OrderedSimpleTable_BST<>();
+		OrderedSimpleTable_BST<Integer, Integer> s = new OrderedSimpleTable_BST<>();
 		// System.out.println(s.size());
 		// s.insert(1);
 		// s.insert(13);
@@ -16,6 +16,8 @@ public class Runner {
 		s.put(10, 9);
 		s.put(21, 6);
 		s.put(16, 1);
+		s.put(1, 1);
+		s.put(3, 1);
 		// s.deleteMax();
 		for (Integer x : s.keys())
 			System.out.println(x + "\t" + s.get(x));
@@ -24,5 +26,36 @@ public class Runner {
 		System.out.println(s.contains(1));
 		System.out.println(s.floor(11));
 		System.out.println(s.ceiling(11));
+		System.out.println("Depth: " + s.depth());
+		// show(s.keys_between(-1, 10));
+		// show(s.keys_level(1));
+		// show(s.keys_all_level());
+
+		
+		System.out.println("*" + s.count_key_only_one_child());
+		System.out.println("*" + s.count_key_have_two_child());
+		show(s.keys_have_two_child());
+		System.out.println("even: " + s.count_even_key());
+		System.out.println("even leaf: " + s.count_even_leaf());
+		show(s.even_leaf());
+		show(s.keys_level(4));
+		System.out.println("Count level: " + s.count_level(4));
+		System.out.println("Sum keys: " + s.sum_keys());
+		System.out.println("Sum leafs: " + s.sum_leaf());
+		System.out.println("Keys have one child:");
+		show(s.keys_only_one_child());
+		System.out.println("Sum node have one child: " + s.sum_keys_one_child());
+		System.out.println("is BST: " + s.is_BST());
+		System.out.println("is blanched BST: " + s.is_blanched_BST());
+		System.out.println("is_completely_balanced_BST: " + s.is_completely_balanced_BST());
+	}
+
+	private static void show(Iterable<Integer> i) {
+		System.out.println("\n********");
+		System.out.println("Keys:");
+		for (Integer x : i)
+			System.out.print(x + " ");
+		System.out.println();
+		System.out.println("---------");
 	}
 }
